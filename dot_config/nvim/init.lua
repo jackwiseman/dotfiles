2f04fa9e36f09b4b496bf6d0fe498dd4ef2d072a
Jack Wiseman <jackwiseman4@gmail.com>
2023-02-13 02:51:41 -0800

    fix(nvim): default language server setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -314,14 +314,9 @@ end
 --
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
--- local servers = {
---   -- clangd = {},
---   -- gopls = {},
---   -- pyright = {},
---   -- rust_analyzer = {},
---   -- tsserver = {},
---   },
--- }
+local servers = {
+  gopls = {},
+}
 
 -- Setup neovim lua configuration
 require('neodev').setup()
