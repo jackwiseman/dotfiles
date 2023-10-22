eb6f42a02bb593536d4ca6a7f823439bd05fd351
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2023-10-21 18:46:07 -0700

    improve shell startup speed, add <leader>-x for trouble

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -455,5 +455,6 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.keymap.set('n', '<leader>f', ":NvimTreeToggle<CR>", { noremap = true, silent = true, desc = "Toggle file explorer" })
+vim.keymap.set('n', '<leader>x', ":TroubleToggle<CR>", { noremap = true, silent = true, desc = "Open trouble.nvim picker" })
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
