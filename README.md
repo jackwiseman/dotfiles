c4af0d8c228250973f8037c15951f5a2db402be1
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-01-16 22:37:26 -0800

    update bootstrapping for monterey

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ```
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply jackwiseman
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jackwiseman/dotfiles/bootstrap.sh)"
 ```
---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# check for existence of homebrew, install if not found
+# this ensures xcode-select is installed as well
+if ! command -v brew &> /dev/null
+then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew install chezmoi
+chezmoi init --apply jackwiseman
