942cfba86b68bddfa054d161afad55f6c5fe0e6d
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-03-22 01:27:43 -0700

    sudo again

--- .chezmoiscripts/linux/run_once_before_dependencies.sh
+++ .chezmoiscripts/linux/run_once_before_dependencies.sh
@@ -14,6 +14,6 @@ done
 # install go 1.21.0
 wget "https://go.dev/dl/go1.22.1.linux-amd64.tar.gz"
 echo "aab8e15785c997ae20f9c88422ee35d962c4562212bb0f879d052a35c8307c7f go1.22.1.linux-amd.tar.gz" | sha256sum -c
-rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 
 # chsh -s "$(which zsh)"
