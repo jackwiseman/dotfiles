d9033354dea5d290a29ec74fbe92028bf0c86450
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-03-22 03:00:58 -0700

    silent install

--- .chezmoiscripts/linux/run_before_dependencies.sh
+++ .chezmoiscripts/linux/run_before_dependencies.sh
@@ -7,15 +7,17 @@ packages=(
 )
 
 echo "Updating dependencies"
-sudo apt update > /dev/null 2>&1;
+sudo apt update > /dev/null 2>&1
 
 for package in "${packages[@]}"
-    do sudo apt install -y "$package"
+    echo "Installing $package"
+    do sudo apt install -y "$package" > /dev/null 2>&1
 done
 
 if ! command -v go > /dev/null 2>&1; then
         version="1.22.1"
-        wget "https://go.dev/dl/go$version.linux-amd64.tar.gz"
+        echo "Installing go $version"
+        wget "https://go.dev/dl/go$version.linux-amd64.tar.gz" > /dev/null 2>&1
         # echo "aab8e15785c997ae20f9c88422ee35d962c4562212bb0f879d052a35c8307c7f go1.22.1.linux-amd.tar.gz" | sha256sum -c
         sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
         rm "go$version.linux-amd64.tar.gz"
@@ -24,7 +26,8 @@ fi
 # install fnm 1.35.1
 if ! command -v fnm > /dev/null 2>&1; then
     version="v1.35.1"
-    wget "https://github.com/Schniz/fnm/releases/download/$version/fnm-linux.zip"
+    echo "Installing fnm $version"
+    wget "https://github.com/Schniz/fnm/releases/download/$version/fnm-linux.zip" > /dev/null 2>&1
     unzip fnm-linux.zip
     rm fnm-linux.zip
     sudo chmod +x fnm
@@ -32,13 +35,15 @@ if ! command -v fnm > /dev/null 2>&1; then
 fi
 
 if ! command -v nvim > /dev/null 2>&1; then
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    echo "Installing neovim"
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz > /dev/null 2>&1
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
 fi
 
 if ! command -v fzf > /dev/null 2>&1; then
     version="0.48.1"
+    echo "Installing fzf $version"
     wget "https://github.com/junegunn/fzf/releases/download/$version/fzf-$version-linux_amd64.tar.gz"
     sudo tar -C /usr/local/bin -xzf "fzf-$version-linux_amd64.tar.gz"
 fi
