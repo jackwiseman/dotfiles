1dfef74bfdb5664a1da8ec9d13f1b76ad64dfe6d
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-03-22 01:36:16 -0700

    start fnm

--- .chezmoiscripts/linux/run_once_before_dependencies.sh
+++ .chezmoiscripts/linux/run_once_before_dependencies.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 packages=(
-    "neovim"
+    # "neovim"
+    "unzip"
     "zsh"
 )
 
@@ -15,5 +16,8 @@ done
 wget "https://go.dev/dl/go1.22.1.linux-amd64.tar.gz"
 # echo "aab8e15785c997ae20f9c88422ee35d962c4562212bb0f879d052a35c8307c7f go1.22.1.linux-amd.tar.gz" | sha256sum -c
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+
+wget "https://github.com/Schniz/fnm/releases/download/v1.35.1/fnm-arm64.zip"
+unzip fnm-arm64.zip
 
 # chsh -s "$(which zsh)"
