14f017a13391543816a40ea6e6042aab37a1aa23
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-03-22 01:56:01 -0700

    update installs

--- .chezmoiscripts/linux/run_once_before_dependencies.sh
+++ .chezmoiscripts/linux/run_once_before_dependencies.sh
@@ -12,17 +12,22 @@ for package in "${packages[@]}"
     do sudo apt install -y "$package"
 done
 
-# install go 1.21.0
-if [ "$(which go)" == 1 ]; then
-        wget "https://go.dev/dl/go1.22.1.linux-amd64.tar.gz"
+if ! command -v go > /dev/null 2>&1; then
+        version="1.22.1"
+        wget "https://go.dev/dl/go$version.linux-amd64.tar.gz"
         # echo "aab8e15785c997ae20f9c88422ee35d962c4562212bb0f879d052a35c8307c7f go1.22.1.linux-amd.tar.gz" | sha256sum -c
-        sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
+        sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
+        rm "go$version.linux-amd64.tar.gz"
 fi
 
 # install fnm 1.35.1
 if ! command -v fnm > /dev/null 2>&1; then
-    wget "https://github.com/Schniz/fnm/releases/download/v1.35.1/fnm-arm64.zip"
-    unzip fnm-arm64.zip
+    version="v1.35.1"
+    wget "https://github.com/Schniz/fnm/releases/download/$version/fnm-linux.zip"
+    unzip fnm-linux.zip
+    rm fnm-linux.zip
+    sudo chmod +x fnm
+    mv fnm /usr/local/bin/fnm
 fi
 
 # chsh -s "$(which zsh)"
