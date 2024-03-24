0496bb412e5f3c4c53d9c6ba1f62c4e6ad4f6722
jack wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-03-24 05:46:21 -0700

    add ephemeral tag, set zsh

--- .chezmoiscripts/linux/run_before_dependencies.sh
+++ .chezmoiscripts/linux/run_before_dependencies.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 packages=(
-    # "neovim"
     "unzip"
     "zsh"
     "gcc"
@@ -16,7 +15,7 @@ done
 
 if ! command -v go > /dev/null 2>&1; then
         version="1.22.1"
-        echo "Installing go"
+        echo "Installing go $version"
         wget "https://go.dev/dl/go$version.linux-amd64.tar.gz" > /dev/null 2>&1
         # echo "aab8e15785c997ae20f9c88422ee35d962c4562212bb0f879d052a35c8307c7f go1.22.1.linux-amd.tar.gz" | sha256sum -c
         sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf "go$version.linux-amd64.tar.gz"
@@ -26,16 +25,16 @@ fi
 # install fnm 1.35.1
 if ! command -v fnm > /dev/null 2>&1; then
     version="v1.35.1"
-    echo "Installing fnm"
+    echo "Installing fnm $version"
     wget "https://github.com/Schniz/fnm/releases/download/$version/fnm-linux.zip" > /dev/null 2>&1
-    unzip fnm-linux.zip
+    unzip fnm-linux.zip > /dev/null 2>&1
     rm fnm-linux.zip
     sudo chmod +x fnm
     sudo mv fnm /usr/local/bin/fnm
 fi
 
 if ! command -v nvim > /dev/null 2>&1; then
-    echo "Installing neovim"
+    echo "Installing latest neovim release"
     wget "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz" > /dev/null 2>&1
     sudo rm -rf /opt/nvim
     sudo tar -C /opt -xzf nvim-linux64.tar.gz
@@ -49,4 +48,3 @@ if ! command -v fzf > /dev/null 2>&1; then
     sudo tar -C /usr/local/bin -xzf "fzf-$version-linux_amd64.tar.gz"
     sudo rm "fzf-$version-linux_amd64.tar.gz"
 fi
-# chsh -s "$(which zsh)"
