e55f5d0c0235311d2150269a2c70c2b19c7c0ef7
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-06-25 21:27:44 -0700

    luarocks, providers

--- dot_config/nvim/lua/jackwiseman/core/init.lua
+++ dot_config/nvim/lua/jackwiseman/core/init.lua
@@ -68,3 +68,9 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 vim.opt.fixeol = false
+
+-- disable all the providers (i've never used them?)
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
