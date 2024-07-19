ac0ac94c5224734de71aea8f966acbcdeab78b60
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-07-18 20:04:37 -0700

    add nixos

---
+++ nixos/flake.nix
@@ -0,0 +1,19 @@
+# /etc/nixos/flake.nix
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = inputs@{ self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+	{
+	  nix = {
+	    settings.experimental-features = [ "nix-command" "flakes" ];
+	  };
+	}
+      ];
+    };
+  };
+}
