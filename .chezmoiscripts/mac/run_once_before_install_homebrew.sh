678eef250d42c860f14284988b029ef906919bc7
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-07-20 21:47:26 -0700

    ensure nix on all hosts

--- .chezmoiscripts/mac/run_once_before_install_homebrew.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# check for existence of homebrew, install if not found
-# this ensures xcode-select is installed as well
-if ! command -v brew &> /dev/null
-then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
