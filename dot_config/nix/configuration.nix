faea407cdf08359cd5195be8b55152eafcde45a4
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-10-21 22:46:16 -0700

    nix: add podman compose

--- dot_config/nix/configuration.nix
+++ dot_config/nix/configuration.nix
@@ -55,7 +55,7 @@
     # dive # look into docker image layers
     # podman-tui # status of containers in the terminal
     # docker-compose # start group of containers for dev
-    # #podman-compose # start group of containers for dev
+    podman-compose # start group of containers for dev
   ];
 
 # virtualisation.docker.enable = true;
