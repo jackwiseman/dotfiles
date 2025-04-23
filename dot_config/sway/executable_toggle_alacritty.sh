2dd5945fa2688f2872256148b43ceb451732081b
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2025-04-22 22:36:17 -0400

    update sway config

--- dot_config/sway/executable_toggle_alacritty.sh
+++ dot_config/sway/executable_toggle_alacritty.sh
@@ -1,4 +1,4 @@
-#!/run/current-system/sw/bin/bash
+#!/bin/bash
 TERM_PIDFILE="/tmp/20718ddc-aa8a-45d8-8a97-a48578c146b0"
 TERM_PID="$(<"$TERM_PIDFILE")"
 if swaymsg "[ pid=$TERM_PID ] scratchpad show"
@@ -10,5 +10,5 @@ else
     echo "$$" > "$TERM_PIDFILE"
     swaymsg "for_window [ pid=$$ ] 'floating enable ; resize set 100ppt 50ppt ; move position 0 0 ; move to scratchpad ; scratchpad show'"
     # swaymsg "for_window [ pid=$$ ] 'floating enable ; resize set 100ppt 100ppt ; move position 0 0 ; move to scratchpad ; scratchpad show'"
-    exec alacritty -e zsh -c "tmux attach || tmux new-session -s 'default'"
+    exec alacritty -e zsh -c "tmux attach -t scratchpad || tmux new-session -s 'scratchpad'"
 fi
