bd4c5207742a43e4ed426cba805b297626922152
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2025-05-27 16:34:59 -0400

    disable copilot tab completion

--- dot_config/nvim/lua/jackwiseman/plugins/copilot.lua
+++ dot_config/nvim/lua/jackwiseman/plugins/copilot.lua
@@ -1,7 +1,11 @@
 return {
 	'https://github.com/github/copilot.vim',
 	lazy = false,
+	init = function()
+		vim.g.copilot_no_tab_map = true
+	end,
 	config = function()
 		vim.keymap.set('i', '<C-V>', '<Plug>(copilot-accept-line)')
 	end
+
 }
