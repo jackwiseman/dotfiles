a4021807faaeb910baa3e113a7020d6da33ac6f2
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2025-12-18 14:06:40 -0500

    feat: disable brew analytics

--- .chezmoiscripts/mac/run_once_before_install_homebrew.sh
+++ .chezmoiscripts/mac/run_once_before_install_homebrew.sh
@@ -2,7 +2,7 @@
 
 # check for existence of homebrew, install if not found
 # this ensures xcode-select is installed as well
-if ! command -v brew &> /dev/null
-then
+if ! command -v brew > /dev/null 2&>1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew analytics off
 fi
