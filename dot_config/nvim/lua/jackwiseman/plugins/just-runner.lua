0f49c37bb0cd26b16a1ca993649d0d0a6fc95826
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2025-12-18 13:12:03 -0500

    chore(nvim): keep justrunner output visible

--- dot_config/nvim/lua/jackwiseman/plugins/just-runner.lua
+++ dot_config/nvim/lua/jackwiseman/plugins/just-runner.lua
@@ -5,9 +5,9 @@ return {
 		require("just-runner").setup({
 			picker = "snacks",
 			window_position = "bottom",
-			close_on_success = true,
+			close_on_success = false,
 			close_on_error = false,
-			pause_before_close = 2000,
+			pause_before_close = 200000,
 		})
 	end,
 	keys = {
