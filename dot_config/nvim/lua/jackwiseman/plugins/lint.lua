a8879bdf5177386424b5afc6753f2ee5278b42bd
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2026-09-14 23:44:16 -0400

    chore: remove eslint

--- dot_config/nvim/lua/jackwiseman/plugins/lint.lua
+++ dot_config/nvim/lua/jackwiseman/plugins/lint.lua
@@ -1,32 +1,17 @@
 return {
 	'mfussenegger/nvim-lint',
 	-- events = { 'BufEnter, BufWritePost, InsertLeave' },
-	ft = { 'sh', 'javascript' },
+	ft = { 'sh' },
 	config = function()
 		local lint = require('lint')
-		local eslint = lint.linters.eslint_d
 
 		lint.linters_by_ft = {
 			sh = { 'shellcheck' },
-			javascript = { 'eslint_d' },
-		}
-
-		eslint.args = {
-			-- '--no-eslintrc',
-			-- '--config',
-			-- vim.fn.expand('$HOME/.eslintrc.js'),
-			'--format',
-			'json',
-			'--stdin',
-			'--stdin-filename',
-			function()
-				return vim.api.nvim_buf_get_name(0)
-			end,
 		}
 
 		vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
 			group = vim.api.nvim_create_augroup('lint', { clear = true }),
-			pattern = { '*.sh', '*.js' },
+			pattern = { '*.sh' },
 			callback = function()
 				lint.try_lint()
 			end,
